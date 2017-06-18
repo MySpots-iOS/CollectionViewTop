f53835f79014b10bfcfbdae6b41624a658a61c9e
Saayaman <ayakosayama1987@gmail.com>
2017-06-17 19:57:11 -0700

    Collection view does not work

---
+++ CollectionViewTop/CollectionViewTop/ViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  CollectionViewTop
+//
+//  Created by ayako_sayama on 2017-06-17.
+//  Copyright © 2017 ayako_sayama. All rights reserved.
+//
+
+import UIKit
+
+class TopPageViewController: UICollectionViewController{
+    
+    
+    private let cellid = "cellid"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView?.backgroundColor = UIColor.white
+        collectionView?.register(CategoryCell.self, forCellWithReuseIdentifier: cellid)
+        view.addSubview(collectionView!)
+    }
+    
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellid, for: indexPath) as! CategoryCell
+        return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout,sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        return CGSize(width: view.frame.width, height: 154)
+//    }
+}
+
+
+class CategoryCell: UICollectionViewCell{
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setUpViews(){
+        backgroundColor = UIColor.red
+    }
+}
+
