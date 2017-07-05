37dfa22ec2f30da4ebf0dc59affbc461fd74e4c1
Saayaman <ayakosayama1987@gmail.com>
2017-07-04 20:09:44 -0700

    Solve getting “Spots” from Firebase

--- CollectionView3/CollectionView3/DataSource.swift
+++ CollectionView3/CollectionView3/DataSource.swift
@@ -1,6 +1,7 @@
 
 import Foundation
 import Firebase
+import GoogleMaps
 
 class DataSource {
     
@@ -20,8 +21,8 @@ class DataSource {
     
     func numberOfSpots(_ index: Int) -> Int{
         
-        if folders[index].spots?.count != nil{
-            return (folders[index].spots?.count)!
+        if folders[index].spots.count != nil{
+            return (folders[index].spots.count)
         }
         return 0
     }
@@ -34,38 +35,22 @@ class DataSource {
         return folders[index].imageName!
     }
     
+    func getFolders(folderIndex: IndexPath) -> [Folder]{
+        return [folders[folderIndex[1]]]
+    }
+    
     
     func firstInit(){
 
         self.ref.child(firebasePath).observe(.value, with: { (snapshot) in
 //        self.ref.child(firebasePath).observeSingleEvent(of: .value, with: { (snapshot) in
             
-            
             for folder in snapshot.children{
                 let newFolder: Folder?
-                
-                print("Readme")
-
-            
+        
                 if let snap = folder as? DataSnapshot {
                     newFolder = self.makeFolder(folder: snap)
                     self.folders.append(newFolder!)
-                    
-                    print(newFolder?.imageName ?? 0)
-                    
-//                    if let snapSpots = (folder as? DataSnapshot)?.childSnapshot(forPath: "Spot") {
-//                        
-//                        for spot in snapSpots.children{
-//                            if let snap = spot as? DataSnapshot{
-//                                let newSpot = self.makeSpot(snap)
-//                                newFolder?.spots?.append(newSpot)
-//                                
-//                                print(newSpot.spotName ?? "noname")
-//                            }
-//                            
-//                        }
-//                        
-//                    }
                 }
 
             }
@@ -74,9 +59,11 @@ class DataSource {
         }) { (error) in
             print(error.localizedDescription)
         }
+  
     }
-   
-    
+
+
+
     func makeFolder(folder:DataSnapshot) -> Folder {
         
         let newFolder = Folder()
@@ -97,27 +84,26 @@ class DataSource {
         
 
         let spots = folder.childSnapshot(forPath: "Spots")
-        print(spots.children)
 
+        for spot in spots.children{
 
-//        for spot in spots.children{
-//            if let snap = spot as? DataSnapshot{
-//                let newSpot = makeSpot(snap)
-//                newFolder.spots?.append(newSpot)
-//                
-//                print(newSpot.spotName ?? "noname")
-//            }
-//            
-//            
-//        }
-//        
-//        print(newFolder.spots?.count ?? 0)
-        
+            guard let snap = spot as? DataSnapshot else{
+                print("ohh nooo")
+                return spot as! Folder
+            }
+            
+            let newSpot:Spot = makeSpot(snap)!
+            newFolder.spots.append(newSpot)
+
+            print("newspot1:\(newSpot)")
+            print("newspot2:\(newFolder.spots)")
+        }
         return newFolder
     }
+    
 
     
-    func makeSpot(_ spots:DataSnapshot) -> Spot{
+    func makeSpot(_ spots:DataSnapshot) -> Spot? {
         
         let newSpot = Spot()
         let value = spots.value as? NSDictionary
@@ -127,69 +113,101 @@ class DataSource {
             newSpot.folderID = folderID as? NSNumber
         }
         
-        if let latitude = value?["latitude"] {
-            newSpot.latitude = latitude as? Double
+        if let placeID = value?["placeID"]{
+            newSpot.placeID = placeID as? String
         }
         
-        if let longitude = value?["longitude"] {
-            newSpot.longitude = longitude as? Double
+
+        guard let spotName = value!["spotName"] as? String else {
+            return nil
         }
+        
+        newSpot.spotName  = spotName
+        
+
+        guard let latitude =  value?["latitude"] as? Double else {
+            return nil
+        }
+        
+        newSpot.latitude = latitude
+
+        
+        if let longitude = value?["longitude"] {
+            newSpot.longitude = (longitude as? Double)!
+        }
+        
+        print("makeSpot: \(String(describing: newSpot.longitude!))")
         
         return newSpot
     }
     
+    
+    func makeMarker(mapView: GMSMapView, folderIndex: Int){
+        
+        
+        //バンクーバーあたりのカフェを入れておいた。placeIDも正確なはず。
+        
+        let map1 = CLLocationCoordinate2D.init(latitude: 49.288102, longitude: -123.113128)
+        let placeID1 = "ChIJ32puLoJxhlQRusnG9dNlmzU"
+        
+        let map2 = CLLocationCoordinate2D.init(latitude: 49.288826, longitude: -123.114628)
+        let placeID2 = "ChIJ32puLoJxhlQRusnG9dNlmzU"
+        
+        let map3 =  CLLocationCoordinate2D.init(latitude: 49.280515, longitude: -123.116851)
+        let placeID3 = "ChIJ32puLoJxhlQRusnG9dNlmzU"
+        
+        
+        let data:Folder? = folders[folderIndex]
+        
+        if (data != nil){
+            print("data: \(String(describing: data!.folderName))")
+            print("spots: \(String(describing: data!.spots))")
+            
+//                for spot in (data?.spots!)!{
+//                    print("spot: \(String(describing: spot.spotName))")
+//                }
+            
+        }
+        
+        
+//        let data = self.folders[1]
+//        
+//        if data != nil{
+//            print("folders!:\(String(describing: data.folderName)) ")
+//            
+//            for spot in data.spots!{
+//                print("spot: \(spot.spotName)")
+//            }
+//        } else{
+//            print("not there")
+//        }
+        
+
+        
+        let marker = GMSMarker(position: map1)
+        marker.snippet = placeID1
+        marker.icon = GMSMarker.markerImage(with: UIColor.black)
+        marker.map = mapView
+        
+        let marker2 = GMSMarker(position: map2)
+        marker2.snippet = placeID2
+        marker2.icon = GMSMarker.markerImage(with: UIColor.black)
+        marker2.map = mapView
+        
+        let marker3 = GMSMarker(position: map3)
+        marker3.snippet = placeID3
+        marker3.icon = GMSMarker.markerImage(with: UIColor.black)
+        marker3.map = mapView
+        
+        // TODO set flag which is stored or not
+        marker.userData = "test"
+        marker2.userData = "test"
+        marker3.userData = "test"
+
+        //        return [marker, marker2, marker3]
+    }
+    
+
+    
 }
-    // MARK:- FruitsForEachGroup
-    
-//    func spotsInFolder(_ index: Int) -> [Spot] {
-//        let item = folders[index]
-//        let filteredSpots = spots.filter { (spot: Spot) -> Bool in
-//            return spot.folderID == item
-//        }
-//        return filteredSpots
-//    }
-    
-    // MARK:- Add Dummy Data
-    
-//    func addAndGetIndexForNewItem() -> Int {
-//        
-//        let spot = Spot()
-//        
-//        let count = spotsInFolder(0).count
-//        
-//        let index = count > 0 ? count - 1 : count
-//        spots.insert(spot, at: index)
-//        
-//        return index
-//    }
-    
-    // MARK:- Delete Items
-    
-//    func deleteItems(_ items: [Fruit]) {
-//        
-//        for item in items {
-//            // remove item
-//            let index = fruits.indexOfObject(item)
-//            if index != -1 {
-//                fruits.remove(at: index)
-//            }
-//        }
-//    }
-    
-    
-
-
-//extension Array {
-//    func indexOfObject<T:AnyObject>(_ item:T) -> Int {
-//        var index = -1
-//        for element in self {
-//            index += 1
-//            if item === element as? T {
-//                return index
-//            }
-//        }
-//        return index
-//    }
-//}
-
-
+ 
